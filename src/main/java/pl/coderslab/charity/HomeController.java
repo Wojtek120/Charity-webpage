6494695f2f60ef7bc78396ad1933ea410d1185d9
Wojciech Buczko <Wojtek120PL@gmail.com>
2019-10-01 13:55:29 +0200

    Initial commit

---
+++ src/main/java/pl/coderslab/charity/HomeController.java
@@ -0,0 +1,17 @@
+package pl.coderslab.charity;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class HomeController {
+
+
+    @RequestMapping("/")
+    public String homeAction(Model model){
+        return "index";
+    }
+}
